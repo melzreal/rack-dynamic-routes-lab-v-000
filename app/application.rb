e67440ae33a522f9ba3b890b023ef664cb4ba911
Melissa Real <melissa.real@hotmail.com>
2018-10-25 17:57:23 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -11,7 +11,7 @@ class Application
           resp.write "You requested #{item.price}"
       elsif item == nil
           resp.write "Item not found"
-          resp.status = 404
+          resp.status = 400
         end
       else
         resp.write "Route not found"
