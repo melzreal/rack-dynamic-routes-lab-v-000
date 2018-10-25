9e379075deaac114d6ec788172d29e62d146aa77
Melissa Real <melissa.real@hotmail.com>
2018-10-25 17:56:07 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -7,13 +7,11 @@ class Application
     if req.path.match(/items/)
       item_name = req.path.split("/items/").last
       item = @@items.find{|i| i.name == item_name}
-        if item !== nil
+      item !== nil
           resp.write "You requested #{item.price}"
-        else
+      elsif item == nil
           resp.write "Route not found"
-          resp.status = 404
-
-        end
+          resp.status = 404      
       end
 
     resp.finish
